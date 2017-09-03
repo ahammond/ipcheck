94c4040e06ce2e9bef5c928234158422dc2bb62c
Andrew Hammond <ahammond@anchorfree.com>
2017-09-02 20:40:27 -0700

    Refactor for simplicity
    I love the cleanlieness of using a specific type with a lookup map,
    but it's overkill.

--- ipcheck_test.go
+++ ipcheck_test.go
@@ -7,63 +7,98 @@ import (
 var testCases = []struct {
 	description string
 	input       string
-	expected    IpType
+	expected    string
 }{
 	{
 		"malformed",
 		"Tom-ay-to, tom-aaaah-to.",
-		malformed,
+		"malformed",
 	},
 	{
 		"public",
 		"132.15.129.2",
-		public,
+		"public",
 	},
 	{
+		"private10",
+		"10.168.10.2",
 		"private",
+	},
+	{
+		"private172",
+		"172.16.10.2",
+		"private",
+	},
+	{
+		"private192",
 		"192.168.10.2",
-		private,
+		"private",
+	},
+	{
+		"private100",
+		"192.168.10.2",
+		"private",
+	},
+	{
+		"private198",
+		"198.18.10.2",
+		"private",
 	},
 	{
 		"loopback",
 		"127.0.0.1",
-		loopback,
+		"loopback",
+	},
+	{
+		"loopback0",
+		"0.0.0.0",
+		"loopback",
 	},
 	{
 		"multicast",
 		"224.0.0.1",
-		multicast,
+		"multicast",
 	},
 	{
 		"linklocal",
 		"169.254.0.1",
-		linklocal,
+		"linklocal",
 	},
 	{
 		"6to4",
 		"192.88.99.1",
-		six2four,
+		"six2four",
 	},
 	{
+		"documentation192",
+		"192.0.2.1",
 		"documentation",
+	},
+	{
+		"documentation198",
+		"198.51.100.1",
+		"documentation",
+	},
+	{
+		"documentation203",
 		"203.0.113.1",
-		documentation,
+		"documentation",
 	},
 	{
 		"reserved",
 		"240.0.0.1",
-		reserved,
+		"reserved",
 	},
 }
 
 func TestCategorize(t *testing.T) {
 	for _, tc := range testCases {
-		actual := Categorize(tc.input)
+		actual := IpCheck(tc.input)
 		if actual != tc.expected {
 			msg := `
 	%s: %s
-	got: %d
-	expected: %d`
+	got: %s
+	expected: %s`
 			t.Fatalf(msg, tc.description, tc.input, actual, tc.expected)
 		}
 	}
