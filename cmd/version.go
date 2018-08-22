d87aa44bd18bb2688bfd0d5e464e0471ea521978
Andrew Hammond <andrew.hammond@helix.com>
2018-08-22 10:55:07 -0700

    support version cmd

---
+++ cmd/version.go
@@ -0,0 +1,25 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+)
+
+var (
+	Version 	string
+	BuildStamp 	string
+	SHA 		string
+)
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number of ipcheck",
+	Long:  `All software has versions. ipcheck has a build timestamp and a git SHA too!`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("%#v %#v %#v\n", Version, BuildStamp, SHA)
+	},
+}
